54a66d5fc434920ad9cc4c1b8a9791b834c1f7e1
Ubaka Ugonna <40705982+Ugonna1054@users.noreply.github.com>
2019-08-28 10:09:54 +0100

    '5'

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,9 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true})
+    const db = config.get('db');
+    mongoose.connect(db, {useNewUrlParser: true})
     .then(_ => winston.info('Connected to vidly db......'))
 }
